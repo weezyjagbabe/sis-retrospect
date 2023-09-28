616f399ca7ecbc158a2121ecb80de38bd9c4a425
weezyjagbabe <borteys@yahoo.com>
2023-09-28 23:30:55 +0100

    Github action test fix

--- src/main/java/com/sis/scrum/retrospect/RetrospectiveApplication.java
+++ src/main/java/com/sis/scrum/retrospect/RetrospectiveApplication.java
@@ -5,7 +5,6 @@ package com.sis.scrum.retrospect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -14,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories("com.sis.scrum.retrospect.repositories")
 @EntityScan("com.sis.scrum.retrospect.models")
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class RetrospectiveApplication {
 
     /**
